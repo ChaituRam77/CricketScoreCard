bebff86a02abcb859be7db26aadf9a69317e1e22
ChaituRam77 <chaitu4tech@gmail.com>
2022-04-02 02:27:51 +0200

    firstCommit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+import { computed } from '@vue/runtime-core'
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <counter />
+    <counter-squared />
+    <buttons />
+    <color-code />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+    'counter': require('@/components/Counter.vue').default,
+    'counter-squared': require('@/components/CounterSquared.vue').default,
+    'buttons': require('@/components/Buttons.vue').default,
+    'color-code': require('@/components/ColorCode.vue').default    
+  }
+}
+
+</script>
+
+<style>
+  div{
+    margin-bottom: 10px;
+  }
+  .counter{
+    font-size: 80px;
+  }
+  .buttons button{
+    font-size: 40px;
+    width: 100px;
+    margin: 0 10px;
+  }
+</style>
